6910d94c612e4162d729979d7e3883790b0071b5
arifin-ilham <ilhamku2005@gmail.com>
2021-09-06 08:06:39 +0700

    commit terakhir

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Alert Box</title>
+    </head>
+    <body>
+        <SCRIPT LANGUAGE = "JavaScript">
+            Window.alert("Selamat Belajar JavaScript");
+            </SCRIPT>
+    </body>
+</html>
